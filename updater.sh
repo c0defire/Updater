8cbf6635a9cd50810f44f68ae88947d4cbc13bcb
c0defire <faustino.javier34@gmail.com>
2020-01-02 08:10:28 -0500

    Add files via upload

---
+++ updater.sh
@@ -0,0 +1,237 @@
+#! /bin/bash
+# ----------------------- DESCRIPTIVE INFORMATION ---------------------------------
+#
+# Updates your system.
+#
+# ----------------------- DESCRIPTIVE INFORMATION ---------------------------------
+#
+# -------------------------- DECLARED VARIABLES -----------------------------------
+#
+#
+# -------------------------- DECLARED VARIABLES -----------------------------------
+#
+#
+os=$(uname -r)  # Name of system
+
+# Determine OS platform
+UNAME=$(uname | tr "[:upper:]" "[:lower:]")
+
+############################################
+# Notes:                                   #
+#                                          #
+# Black        0;30     Dark Gray     1;30 #
+# Red          0;31     Light Red     1;31 #
+# Green        0;32     Light Green   1;32 #
+# Brown/Orange 0;33     Yellow        1;33 #
+# Blue         0;34     Light Blue    1;34 #
+# Purple       0;35     Light Purple  1;35 #
+# Cyan         0;36     Light Cyan    1;36 #
+# Light Gray   0;37     White         1;37 #
+#                                          #
+############################################
+
+#
+# -------------------------------- FUNCTIONS --------------------------------------
+#
+
+# banner - Alias art
+function banner()
+{
+GREEN='\033[0;32m'
+NC='\033[0m' # No color
+
+cat << "EOF"
+
+                                 /h`
+                  `-/+osssso+/-oMMm.
+             `:ohmMMMNNNNNNNNMMMMMMm-
+          `:yNMMNmyo/:-....-+NMMMMMMN/
+        `/dMMNh/.          -dNNNNNNNNN/
+       :dMMmo.             ``````......
+      oNMNs.
+     sMMN/                                 `-
+    +MMN:                                `ymM+
+   `NMMo                                  oMMN`
+   /MMN`                                  `NMM/
+   sMMm                                    dMMs
+   sMMm                                    dMMs
+   /MMN`                                  `NMM/
+   `mMMs                                  sMMm`
+    /MNd.                                /NMM/
+     :.`                                /NMMo
+                                      .yNMN+
+           .--........`             -sNMMh-
+           :mNNNNNNNNd.          -+hNMMh:`
+            :NMMMMMMN+--..--:+shmNMMms-
+             -mMMMMMMNNNNNNNNMMNmy+-`
+              `dMMo.:/+oooo+/:.`
+               .h/
+
+#    # #####  #####    ##   ##### ###### #####
+#    # #    # #    #  #  #    #   #      #    #
+#    # #    # #    # #    #   #   #####  #    #
+#    # #####  #    # ######   #   #      #####
+#    # #      #    # #    #   #   #      #   #
+ ####  #      #####  #    #   #   ###### #    #
+
+Updater v1.2
+Author: C0defire
+For updates run command: wget https://raw.githubusercontent.com/javierfaustino/Shell-Scripts/master/updater.sh
+
+
+EOF
+}
+
+# percentage - Shows percentage at process
+function percentage()
+{
+    echo "================================================"
+    echo "=                $*% COMPLETE                  ="
+    echo "================================================"
+}
+
+# redhat - If system is RedHat then it performs the following instructions
+function redhat() {
+
+percentage 30
+
+echo "Updating: $os"
+sudo yum update -y
+
+sleep 1
+echo
+percentage 60
+
+echo "Upgrading: $os"
+sudo yum upgrade -y
+sleep 1
+echo
+percentage 90
+
+echo "Removing unnecessary software"
+sudo yum autoremove -y
+sleep 1
+echo
+percentage 100
+}
+
+# ubuntu - If system is ubuntu then it performs the following instructions
+function ubuntu() {
+
+echo "Cleaning: $os"
+sudo apt clean
+sleep 1
+echo
+percentage 20
+
+echo "Updating: $os"
+sudo apt update -y
+sleep 1
+echo
+percentage 40
+
+echo "Upgrading: $os"
+sudo apt upgrade -y
+sleep 1
+echo
+percentage 60
+
+echo "Dist-Upgrading: $os"
+sudo apt dist-upgrade -y
+sleep 1
+echo
+percentage 80
+
+echo "Auto removing unwanted programs: $os"
+sudo apt autoremove -y
+echo
+
+percentage 100
+sleep 1
+}
+
+# raspbian - If system is ubuntu then it performs the following instructions
+function raspbian() {
+
+echo "Cleaning: $os"
+sudo apt clean
+sleep 1
+echo
+percentage 20
+
+echo "Updating: $os"
+sudo apt update -y
+sleep 1
+echo
+percentage 40
+
+echo "Upgrading: $os"
+sudo apt upgrade -y
+sleep 1
+echo
+percentage 60
+
+echo "Dist-Upgrading: $os"
+sudo apt dist-upgrade -y
+sleep 1
+echo
+percentage 80
+
+echo "Auto removing unwanted programs: $os"
+sudo apt autoremove -y
+echo
+
+percentage 100
+sleep 1
+}
+
+#
+# -------------------------------- FUNCTIONS --------------------------------------
+#
+
+#
+# ------------------------------ INSTRUCTIONS -------------------------------------
+#
+clear
+echo
+banner
+
+# Below if-statement to be used by newer OS
+if [ "$UNAME" == "linux" ]
+then
+# If available, use LSB to identify distribution
+    export DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
+       # Otherwise, use release info file
+       if [ "$DISTRO" == "CentOS" ]
+       then
+          redhat # Runs the redhat function
+       fi
+
+       if [ "$DISTRO" == "Ubuntu" ]
+       then
+          ubuntu # Runs the ubuntu function
+       fi
+
+	if [ "$DISTRO" == "Kali" ]
+	then
+	ubuntu # Runs the ubuntu function
+	fi
+
+fi
+# Below if-statement to be used by newer OS
+if [ "$UNAME" == "linux" ]
+then
+# If available, use LSB to identify distribution
+    if [ -f /usr/bin/lsb_release  ]
+    then
+       export DISTRO=$(lsb_release -i | cut -d: -f2 | sed s/'^\t'//)
+	if [ "$DISTRO" == "Raspbian" ]
+	then
+	  raspbian # Runs the raspbian function
+        fi
+    fi
+fi
+
+#
+# ------------------------------ INSTRUCTIONS -----------------------------------
+
